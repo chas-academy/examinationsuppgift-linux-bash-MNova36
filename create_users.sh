f31e7edb7cb4bba6bccf177b628aa002568c0b87
M.Nova9 <38569668+MNova36@users.noreply.github.com>
2026-04-24 05:06:42 +0200

    Update create_users.sh

--- create_users.sh
+++ create_users.sh
@@ -12,25 +12,34 @@ fi
 
 for user in "$@"; do
 
-    # skapa användare 
+    # SKAPA ANVÄNDARE 
     useradd -m "$user"
 
-    # home från systemet
-    home=$(eval echo "~$user")
+    home="/home/$user"
 
+    # MAPPAR 
     mkdir -p "$home/Documents"
     mkdir -p "$home/Downloads"
     mkdir -p "$home/Work"
 
+    # ÄGARE 
     chown -R "$user:$user" "$home"
 
+    # RÄTTIGHETER 
     chmod 700 "$home/Documents"
     chmod 700 "$home/Downloads"
     chmod 700 "$home/Work"
 
+    # WELCOME 
     echo "Välkommen $user" > "$home/welcome.txt"
     echo "" >> "$home/welcome.txt"
-    cut -d: -f1 /etc/passwd | grep -v "^$user$" >> "$home/welcome.txt"
+
+    # LISTA ANDRA USERS 
+    for u in "$@"; do
+        if [ "$u" != "$user" ]; then
+            echo "$u" >> "$home/welcome.txt"
+        fi
+    done
 
     chown "$user:$user" "$home/welcome.txt"
     chmod 600 "$home/welcome.txt"
